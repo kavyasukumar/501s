ac366ddb81e55eecb178c0fa32bbd0409fb82225
Kavya Sukumar <kavya.sukumar@outlook.com>
2017-04-06 07:24:10 -0700

    base version

---
+++ source/javascripts/app.js
@@ -0,0 +1,10 @@
+//= require ./_vendor/jquery
+//= require foundation/foundation
+//= require foundation/foundation.reveal
+//= require foundation/foundation.accordion
+
+(function() {
+  $(document).ready(function() {
+    $(document).foundation();
+  });
+})();
